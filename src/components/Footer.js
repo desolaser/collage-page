ba06ecfcd14a2f56c635148f2d28f26014a3dd16
Felipe Olavarria <178.felipe@gmail.com>
2020-05-29 00:11:59 -0400

    Add responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,17 +20,27 @@ const useStyles = makeStyles(theme => ({
         color: 'white',
         borderRadius: 0,
         boxShadow: 0,
-        height: 250
+        height: '100%',
+        [theme.breakpoints.down('sm')]: {
+        }
     },
     paperSecondary: {
         backgroundColor: theme.palette.secondary.main,
         color: 'black',
         borderRadius: 0,
         boxShadow: 0,
-        height: 250
+        height: '100%'
     },
     primaryDark: {
         color: theme.palette.primary.dark
+    },
+    title: {
+        [theme.breakpoints.down('sm')]: {
+            display: 'none'
+        },
+        [theme.breakpoints.up('md')]: {
+            display: 'block'
+        }
     }
 }))
 
@@ -41,31 +51,44 @@ const Footer = () => {
         <Grid container>
             <Grid item xs>
                 <Paper className={classes.paperPrimary}>
-                    <div style={{ padding: 30, display: 'inline-block' }}>
-                        <Typography variant="h5">
-                            Compañía
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            Acerca de nosotros
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            Contacto
-                        </Typography>
-                    </div>
-                    <div style={{ paddingTop: 30, float: 'right', display: 'inline-block' }}>
-                        <Typography variant="h2" color="secondary">
-                            Ima
-                        </Typography>
-                        <Grid container justify="flex-end" spacing={2}>
-                            <Grid item>
-                                <Avatar alt="Facebook" src={FacebookIcon} />
-                            </Grid>
-                            <Grid item style={{ paddingRight: 20 }}>
-                                <Avatar alt="Instagram" src={InstagramIcon} />
+                    <Grid container justify="space-between">
+                        <Grid item xs={0} sm={0} md={4} style={{ padding: 20 }}>
+                            <Typography variant="h5">
+                                Compañía
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                Acerca de nosotros
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                Contacto
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={0} sm={0} md={4} style={{ padding: 20 }}>
+                            <Typography variant="h5">
+                                Dirección
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                Calle Urmeneta
+                            </Typography>
+                            <Typography variant="subtitle2">
+                                Número 334
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={0} sm={0} md={4} style={{ paddingTop: 20 }}>
+                            <Typography className={classes.title} variant="h2" color="secondary" align="right">
+                                Ima
+                            </Typography>
+                            <Grid container justify="flex-end" spacing={2} style={{ padding: 20 }}>
+                                <Grid item>
+                                    <Avatar alt="Facebook" src={FacebookIcon} />
+                                </Grid>
+                                <Grid item style={{ paddingRight: 20 }}>
+                                    <Avatar alt="Instagram" src={InstagramIcon} />
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </div>
-                    <div style={{ marginTop: 30, padding: 25 }}>
+                    </Grid>
+                    <div style={{ padding: 25, marginTop: 20 }}>
                         <Typography variant="body1">
                             Copyright @Discretuum 2020
                         </Typography>
@@ -74,49 +97,43 @@ const Footer = () => {
             </Grid>
             <Grid item xs>
                 <Paper className={classes.paperSecondary}>
-                    <div style={{ padding: 30, float: 'right', display: 'inline-block' }}>
-                        <Typography variant="h5">
-                            Dirección
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            Calle Urmeneta, número 334
-                        </Typography>
-                    </div>
-                    <div style={{ padding: 30, float: 'right', display: 'inline-block' }}>
-                        <Typography variant="h5">
-                            Email
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            imagini@imagini.cl
-                        </Typography>
-                    </div>
-                    <div style={{ padding: 30, float: 'right', display: 'inline-block' }}>
-                        <Typography variant="h5">
-                            Teléfono
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            +596 1234 5678
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            +596 1234 5678
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            +596 1234 5678
-                        </Typography>
-                    </div>
-                    <div style={{ paddingTop: 30, display: 'inline-block' }}>
-                        <Typography className={classes.primaryDark} variant="h2">
-                            gini
-                        </Typography>
-                        <Grid container justify="flex-start" spacing={2}>
-                            <Grid item style={{ paddingLeft: 20 }}>
-                                <Avatar alt="LinkedIn" src={LinkedInIcon} />
-                            </Grid>
-                            <Grid item>
-                                <Avatar alt="Twitter" src={TwitterIcon} />
+                    <Grid container justify="space-between">
+                        <Grid item xs={0} sm={0} md={4} style={{ paddingTop: 20 }}>
+                            <Typography className={`${classes.title} ${classes.primaryDark}`} variant="h2">
+                                gini
+                            </Typography>
+                            <Grid container justify="flex-start" spacing={2} style={{ padding: 20 }}>
+                                <Grid item style={{ paddingLeft: 20 }}>
+                                    <Avatar alt="LinkedIn" src={LinkedInIcon} />
+                                </Grid>
+                                <Grid item>
+                                    <Avatar alt="Twitter" src={TwitterIcon} />
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </div>
+                        <Grid item xs={0} sm={0} md={4} style={{ padding: 20 }}>
+                            <Typography variant="h5" align="right">
+                                Email
+                            </Typography>
+                            <Typography variant="subtitle2" align="right">
+                                imagini@imagini.cl
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={0} sm={0} md={4} style={{ padding: 20 }}>
+                            <Typography variant="h5" align="right">
+                                Teléfono
+                            </Typography>
+                            <Typography variant="subtitle2" align="right">
+                                +596 1234 5678
+                            </Typography>
+                            <Typography variant="subtitle2" align="right">
+                                +596 1234 5678
+                            </Typography>
+                            <Typography variant="subtitle2" align="right">
+                                +596 1234 5678
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Paper>
             </Grid>
         </Grid>
